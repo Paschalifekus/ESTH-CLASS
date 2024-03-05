6ccb27097f9ccaf2e55ac13bd24ebfc4ccc5db59
Paschalifekus <Paschalifekus@gmail.com>
2024-03-05 15:22:08 +0100

    Initial Commit

--- ecommerce.js
+++ ecommerce.js
@@ -2,103 +2,103 @@ const prompt = require('prompt-sync')({sigint: true})
 
  ecommerceProduct  = [
     {
-        productId: "1",
+        productId: 1,
         productName: "sugar",
         productQuantity: 20,
         productPrice: 4000,
     
     },
     {
-        productId: "2",
+        productId: 2,
         productName: "garri",
         productQuantity: 15,
         productPrice: 8000,
     
     },
     {
-        productId: "3",
+        productId: 3,
         productName: "beans",
         productQuantity: 5,
         productPrice: 4000,
     
     },
     {
-        productId: "4",
+        productId: 4,
         productName: "coke",
         productQuantity: 10,
         productPrice: 10000,
     
     },
     {
-        productId: "5",
+        productId: 5,
         productName: "yam",
         productQuantity: 16,
         productPrice: 6000,
     
     },
     {
-        productId: "6",
+        productId: 6,
         productName: "rice",
         productQuantity: 25,
         productPrice: 7600,
     },
     {
-        productId: "7",
+        productId: 7,
         productName: "bread",
         productQuantity: 10,
         productPrice: 5000,
     },
     {
-        productId: "8",
+        productId: 8,
         productName: "juice",
         productQuantity: 23,
         productPrice: 7600,
     
     },
     {
-        productId: "9",
+        productId: 9,
         productName: "onions",
         productQuantity: 34,
         productPrice: 8000,
     
     },
     {
-        productId: "10",
+        productId: 10,
         productName: "egg",
         productQuantity: 34,
         productPrice: 7700,
     
     },
     {
-        productId: "11",
+        productId: 11,
         productName: "corn",
         productQuantity: 45,
         productPrice: 4500,
     
     },
     {
-        productId: "12",
+        productId: 12,
         productName: "wheat",
         productQuantity: 17,
         productPrice: 14000,
     
     },
     {
-        productId: "13",
+        productId: 13,
         productName: "garlic",
         productQuantity: 13,
         productPrice: 45000,
     
     },
     {
-        productId: "14",
+        productId: 14,
         productName: "ginger",
         productQuantity: 17,
         productPrice: 45000,
     
     },
     {
-        productId: "15",
+        productId: 15,
         productName: "pepper",
         productQuantity: 30,
         productPrice: 4300,
@@ -140,7 +140,7 @@ function displayCart(){
     }
 
     // using the the reduce function to get the total price of the items in the cart
-    const total = cart.reduce((tottal, item) => tottal + item.productPrice * item.quantity, 0) 
+    const total = cart.reduce((total, item) => total + item.productPrice * item.quantity, 0) 
     console.log(`The total price of the Products you puchased is $ ${total}`)
 }
 
